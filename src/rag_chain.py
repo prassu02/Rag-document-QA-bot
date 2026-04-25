c631bf8911868ff34ee3644c058ea9a246ff5817
Prasanna kumar <prasuprasanna8978l@gmail.com>
2026-04-26 01:26:55 +0530

    first commit

--- src/rag_chain.py
+++ src/rag_chain.py
@@ -1,20 +1,23 @@
 import os
-import streamlit as st
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from src.retriever import retrieve_documents
 
 load_dotenv()
 
-def get_llm():
 
-    api_key = st.secrets["GROQ_API_KEY"]
+def get_llm():
+    api_key = os.getenv("GROQ_API_KEY")
+
+    if not api_key:
+        raise ValueError("GROQ_API_KEY not found in .env file")
 
     return ChatGroq(
         groq_api_key=api_key,
         model_name="llama-3.3-70b-versatile",
         temperature=0
     )
+
 
 def answer_question(query):
     docs = retrieve_documents(query)
