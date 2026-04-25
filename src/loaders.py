22003e7c0a3aa6a3aa036e139fdc26fb9eba28b5
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-04-26 00:38:49 +0530

    Update loaders.py

--- src/loaders.py
+++ src/loaders.py
@@ -1,14 +1,39 @@
-from langchain_community.document_loaders import PyMuPDFLoader
 import os
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+DATA_DIR="data"
+
 
 def load_documents():
+
     docs=[]
 
-    for file in os.listdir("data"):
-        if file.endswith(".pdf"):
-            loader = PyMuPDFLoader(
-                os.path.join("data", file)
-            )
-            docs.extend(loader.load())
+    if not os.path.exists(DATA_DIR):
+        raise FileNotFoundError(
+            "data folder not found"
+        )
 
-    return docs
+    pdf_files=[
+        f for f in os.listdir(DATA_DIR)
+        if f.endswith(".pdf")
+    ]
+
+    if len(pdf_files)==0:
+        raise ValueError(
+            "No PDF files in data folder"
+        )
+
+    for file in pdf_files:
+        path=os.path.join(DATA_DIR,file)
+        loader=PyMuPDFLoader(path)
+        docs.extend(loader.load())
+
+    splitter=RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks=splitter.split_documents(docs)
+
+    return chunks
