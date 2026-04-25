3072396e06e9d0085f373f6872bbfaae3343f9a5
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-04-26 00:18:06 +0530

    Update retriever.py

--- src/retriever.py
+++ src/retriever.py
@@ -2,27 +2,26 @@ import os
 from langchain_community.vectorstores import FAISS
 from src.embeddings import get_embedding_model
 from src.loaders import load_documents
-from src.chunking import chunk_documents
-from src.config import DB_PATH, DATA_PATH, TOP_K
+
+DB_PATH="faiss_index"
 
 def retrieve_documents(query):
     embeddings = get_embedding_model()
 
-    # Build index if missing
-    if not os.path.exists(DB_PATH):
-        docs = load_documents(DATA_PATH)
-        chunks = chunk_documents(docs)
+    try:
+        if os.path.exists(DB_PATH):
+            db = FAISS.load_local(
+                DB_PATH,
+                embeddings,
+                allow_dangerous_deserialization=True
+            )
+        else:
+            docs = load_documents()
+            db = FAISS.from_documents(docs, embeddings)
+            db.save_local(DB_PATH)
 
-        db = FAISS.from_documents(
-            chunks,
-            embeddings
-        )
-        db.save_local(DB_PATH)
+        return db.similarity_search(query,k=4)
 
-    db = FAISS.load_local(
-        DB_PATH,
-        embeddings,
-        allow_dangerous_deserialization=True
-    )
-
-    return db.similarity_search(query, k=TOP_K)
+    except Exception as e:
+        print("ERROR:", e)
+        raise e
