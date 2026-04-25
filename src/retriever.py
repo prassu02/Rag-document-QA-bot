2a37e465401e86ec5879a9f8daa142053523f711
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-04-26 00:40:02 +0530

    Update retriever.py

--- src/retriever.py
+++ src/retriever.py
@@ -5,23 +5,31 @@ from src.loaders import load_documents
 
 DB_PATH="faiss_index"
 
+
 def retrieve_documents(query):
-    embeddings = get_embedding_model()
 
-    try:
-        if os.path.exists(DB_PATH):
-            db = FAISS.load_local(
-                DB_PATH,
-                embeddings,
-                allow_dangerous_deserialization=True
-            )
-        else:
-            docs = load_documents()
-            db = FAISS.from_documents(docs, embeddings)
-            db.save_local(DB_PATH)
+    embeddings=get_embedding_model()
 
-        return db.similarity_search(query,k=4)
+    if os.path.exists(DB_PATH):
+        db=FAISS.load_local(
+            DB_PATH,
+            embeddings,
+            allow_dangerous_deserialization=True
+        )
 
-    except Exception as e:
-        print("ERROR:", e)
-        raise e
+    else:
+        docs=load_documents()
+
+        db=FAISS.from_documents(
+            docs,
+            embeddings
+        )
+
+        db.save_local(DB_PATH)
+
+    results=db.similarity_search(
+        query,
+        k=4
+    )
+
+    return results
